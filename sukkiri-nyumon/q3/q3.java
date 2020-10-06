d9c953885a7e60d42bc49932388bd11a5ae1a43d
ndj <54813841+ndjndj@users.noreply.github.com>
2020-10-07 00:15:29 +0900

    Create q3.java

---
+++ sukkiri-nyumon/q3/q3.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        int weight = 60;
+        if (weight == 60) {
+            System.out.println(weight);
+        }
+        
+        int age1 = 30;
+        int age2 = 20;
+        if ((age1 + age2) * 2 > 60) {
+            System.out.println("over");
+        }
+        
+        int age = 21;
+        if (age % 2 != 0) {
+            System.out.println("odd");
+        }
+        
+        String name = "湊";
+        if (name.equals("湊")) {
+            System.out.println(name);
+        }
+    }
+    
+}
