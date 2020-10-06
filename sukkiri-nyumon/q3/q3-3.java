41e02dbbdac14ba0a8d22cba9b00f552e3e783bd
ndj <54813841+ndjndj@users.noreply.github.com>
2020-10-07 00:20:06 +0900

    Create q3-3.java

---
+++ sukkiri-nyumon/q3/q3-3.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int isHungry = 1;
+        String food = "ラーメン";
+        
+        System.out.println("こんにちは");
+        
+        if(isHungry == 0) {
+            System.out.println("お腹がいっぱいです");
+        } else {
+            System.out.println("はらぺこです");
+        }
+        
+        if(isHungry == 1) {
+            System.out.println(food + "をいただきます。");
+        }
+        
+        System.out.println("ごちそうさまでした");
+    }
+    
+}
